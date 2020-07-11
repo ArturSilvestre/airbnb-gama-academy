ca830b295f4f2349af90d46791f7df0231ea63ae
artur_silvestre <artur.silvestre@audtax.com.br>
2020-07-11 15:10:32 -0300

    feito alterações main.js e index.html

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const API_URL = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72";
 
 // Para isso, usaremos o "fetch", conforme você deve ter visto nas aulas online.
 // Corrija a linha abaixo, para que a requisição funcione.
-XMLHttpRequest(API_URI)
+fetch(API_URI)
   .then(response => response.text())
   .then(result => {
     // Aqui, estamos pegando o resultado da requisição e parseando em JSON, 
@@ -24,7 +24,7 @@ XMLHttpRequest(API_URI)
       div.nomeDaClasse = "col-md-4";
 
       // Boa! Agora, precisamos criar a "div" do card a ser exibido. Ao criar, atribua a class "card mb-4 box-shadow".
-      card = ?
+      // card = ?
 
       // Humm... Agora que já temos o card, que tal começarmos a popular as informações?
       // Crie um elemento "img" e, atribua a class como "card-img-top";
@@ -34,12 +34,12 @@ XMLHttpRequest(API_URI)
 
       // Legal! Agora, já podemos começar a construir o "corpo" do card. Bora lá!
       // Obs.: atribua a class "card-body" do Bootstrap, para que fique bonitão. :)
-      cardBody = ?
+      // cardBody = ?
 
       // Beleza, corpo criado. Seguindo nossa estrutura, o próximo elemento seria os textos, para exibir
       // o tipo da propriedade, nome e preço. Mas, antes, vamos criar um elemento "div" para manter isso
       // tudo organizado. Você pode atribuir a class "card-text" do Bootstrap!
-      cardText = ?
+      // cardText = ?
 
       // Uffa! Finalmente os elementos da proriedade!
       // Precisamos agora, listar o tipo, o nome e o preço.
@@ -47,9 +47,9 @@ XMLHttpRequest(API_URI)
       propertyType.className = "property-type";
       propertyType.innerHTML = element;
 
-      propertyName = ?
+      // propertyName = ?
 
-      propertyPrice = ?
+      // propertyPrice = ?
 
       // AOBA! Temos agora, todos os elementos necessários para percorrer a resposta da API e,
       // anexar um ao outro para exibição ao usuário. Para "anexar" os elementos, nós usaremos
